5f66762ee07cf7ea76616523efa5da01eadcb9b2
NonhlanhlaMazibuko <nonhlanhlamazibuko17@gmail.com>
2024-06-19 12:29:04 +0200

    Resolves #5 - Worked on the functionality  of the pages

--- movie-project/src/App.js
+++ movie-project/src/App.js
@@ -1,12 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Actors from './Pages/Actors';
+import Movies from './Pages/Movies';
+import SingleActor from './Pages/SingleActor';
+import SingleMovie from './Pages/SingleMovie';
 import "./App.css"
 
 function App() {
-  return <div className="App">
-    <Home />
-    <Actors />
-  </div>
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="/actors/:id" element={<SingleActor />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<SingleMovie />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
