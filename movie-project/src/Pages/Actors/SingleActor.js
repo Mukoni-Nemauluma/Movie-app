d64f8ea9fcb8e4e9086cf6cc8a2568141a0ce7ba
Konanani Nemauluma <koniecharity@gmail.com>
2024-06-24 16:19:54 +0200

    styles

--- movie-project/src/Pages/Actors/SingleActor.js
+++ movie-project/src/Pages/Actors/SingleActor.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { Link, useParams } from "react-router-dom"
 import { fetchActorDetails, fetchMoviesByActor } from "../../util/API"
-import "../../Styles.css"
 
 const SingleActor = () => {
   const { actorId } = useParams()
