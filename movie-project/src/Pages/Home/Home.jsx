1d73dae5dcd81afa6bc44a354d72a83c0083993c
Konanani Nemauluma <koniecharity@gmail.com>
2024-06-24 12:51:06 +0200

    updated searchbar

--- movie-project/src/Pages/Home/Home.jsx
+++ movie-project/src/Pages/Home/Home.jsx
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react"
-import { fetchMoviesByCategory } from "../../util/API"
+import {
+  fetchMoviesByCategory,
+  fetchPopularMovies,
+  fetchTopRatedMovies,
+  fetchMoviesByGenre,
+} from "../../util/API"
+import "../../Home.css"
 
 const Home = () => {
-  const [movies, setMovies] = useState([])
+  const [latestMovies, setLatestMovies] = useState([])
+  const [popularMovies, setPopularMovies] = useState([])
+  const [topRatedMovies, setTopRatedMovies] = useState([])
+  const [comedyMovies, setComedyMovies] = useState([])
+  const [crimeMovies, setCrimeMovies] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    const fetchLatestMovies = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetchMoviesByCategory("now_playing")
-        setMovies(response.results)
+        const latestResponse = await fetchMoviesByCategory("now_playing")
+        const popularResponse = await fetchPopularMovies()
+        const topRatedResponse = await fetchTopRatedMovies()
+        const comedyResponse = await fetchMoviesByGenre(35) // Genre ID for Comedy
+        const crimeResponse = await fetchMoviesByGenre(80) // Genre ID for Crime
+
+        setLatestMovies(latestResponse.results)
+        setPopularMovies(popularResponse.results)
+        setTopRatedMovies(topRatedResponse.results)
+        setComedyMovies(comedyResponse.results)
+        setCrimeMovies(crimeResponse.results)
         setLoading(false)
       } catch (error) {
         setError(error.message)
@@ -18,7 +37,7 @@ const Home = () => {
       }
     }
 
-    fetchLatestMovies()
+    fetchData()
   }, [])
 
   if (loading) return <div>Loading...</div>
@@ -26,17 +45,92 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Latest Movies</h1>
-      <div className="movies-grid">
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            <img
-              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-              alt={movie.title}
+      <section className="hero-section">
+        <div className="tv-container">
+          <img
+            className="tv-image"
+            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+            alt=""
+          />
+          <video autoPlay playsInline muted loop className="tv-video">
+            <source
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v"
+              type="video/mp4"
             />
+          </video>
+        </div>
+        <div className="hero-content">
+          <h1>Welcome to Movie Time</h1>
+          <p>
+            Discover a world of entertainment. Watch your favorite movies now.
+          </p>
+          <button className="button">Explore</button>
+        </div>
+      </section>
+
+      <section>
+        <div className="movie-section">
+          <h1>Latest</h1>
+          <div className="movies-grid">
+            {latestMovies.map((movie) => (
+              <div key={movie.id} className="movie-card">
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="movie-section">
+          <h1>Popular</h1>
+          <div className="movies-grid">
+            {popularMovies.map((movie) => (
+              <div key={movie.id} className="movie-card">
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="movie-section">
+          <h1>Top-Rated</h1>
+          <div className="movies-grid">
+            {topRatedMovies.map((movie) => (
+              <div key={movie.id} className="movie-card">
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="movie-section">
+          <h1>Comedy</h1>
+          <div className="movies-grid">
+            {comedyMovies.map((movie) => (
+              <div key={movie.id} className="movie-card">
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
